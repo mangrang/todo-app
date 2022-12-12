52cbd94151195410eab67f70ce1c8abc3c861c1a
DESKTOP-C4630N1\sooy9 <amole2315@naver.com>
2022-12-12 15:16:22 +0900

    feat: Todo 리스트 보이기

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import Todo from "./components/Todo";
+
 const App = () => {
-  return <div className="App">My todo list</div>;
+  const [todoItems, setTodoItems] = useState([
+    {
+      id: 1,
+      title: "My Todo1",
+      done: false,
+    },
+    {
+      id: 2,
+      title: "My Todo2",
+      done: false,
+    },
+    {
+      id: 3,
+      title: "My Todo3",
+      done: true,
+    },
+  ]);
+  return (
+    <div className="App">
+      {todoItems.map((item) => {
+        return <Todo key={item.id} todo={item} />;
+      })}
+    </div>
+  );
 };
 
 export default App;
